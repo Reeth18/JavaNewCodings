4ff2fa9939bc02e7ec1b0a79e67a12ff2b385b0b
Reeth <rayith.bose18@gmail.com>
2023-03-20 13:30:51 +0530

    Slicing a String

---
+++ Substring.java
@@ -0,0 +1,11 @@
+//  WAP in Java to display the substring when the starting index and ending index is given.
+
+public class Substring {
+    public static void main(String[] args) {
+        String Substring1 = "Mangalyaan";
+        System.out.println("subString of Mangalyaan from 0 to 5 is:" + Substring1.substring(0, 6));
+
+        String Substring2 = "Dinosaur";
+        System.out.println("subString of Dinosaur from 4 to 7 is:" + Substring2.substring(4, 8));
+    }
+}
